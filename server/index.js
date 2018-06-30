cf5da4e30f005e738fd274f376a3f1d474b1f45c
Hugo Benjumea <hugo_andresb@hotmail.com>
2018-06-30 13:39:15 -0500

    Clase 30 junio

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const api = require('./api/v1/');
+
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
+// parse application/json
+app.use(bodyParser.json());
+
+app.use('/api/v1', api);
+app.use('/api', api);
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({
+    message: 'Not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500);
+  res.json({
+    message: err.message,
+  });
+});
+
+module.exports = app;
